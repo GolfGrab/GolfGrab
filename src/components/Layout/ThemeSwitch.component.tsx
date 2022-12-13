4e0c14f3bd78e4261d44f91fa4fd13d3fb6a3b92
GolfGrab <surapus01@gmail.com>
2022-12-13 10:13:13 +0700

    ✨ Add next-themes * ➕ Install next-themes dependencies * 🔨 Add ThemeProvider to _app.tsx * 🔨 Add ThemeSwitch component * ⚗️ Test switching themes with ThemeSwitch component

---
+++ src/components/Layout/ThemeSwitch.component.tsx
@@ -0,0 +1,60 @@
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+const ThemeSwitch = () => {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+  const themes = [
+    "system",
+    "light",
+    "dark",
+    "cupcake",
+    "bumblebee",
+    "emerald",
+    "corporate",
+    "synthwave",
+    "retro",
+    "cyberpunk",
+    "valentine",
+    "halloween",
+    "garden",
+    "forest",
+    "aqua",
+    "lofi",
+    "pastel",
+    "fantasy",
+    "wireframe",
+    "black",
+    "luxury",
+    "dracula",
+    "cmyk",
+    "autumn",
+    "business",
+    "acid",
+    "lemonade",
+    "night",
+    "coffee",
+    "winter",
+  ];
+
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <select value={theme} onChange={(e) => setTheme(e.target.value)}>
+      {themes.map((theme) => (
+        <option key={theme} value={theme}>
+          {theme}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default ThemeSwitch;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
+import { ThemeProvider } from "next-themes";
 import { type AppType } from "next/dist/shared/lib/utils";
 
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 };
 
 export default MyApp;
